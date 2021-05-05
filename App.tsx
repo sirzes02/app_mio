ed9570231585a8b6e7d3a0b46eb552bf064d6c1a
sirzes02 <sirzes02@gmail.com>
2021-05-05 18:21:09 -0500

    firt map

--- App.tsx
+++ App.tsx
@@ -1,18 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, PermissionsAndroid, StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const App = () => {
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+
+  const checkPermissions = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Mio APP Camera Permission',
+        message: 'Mio App needs access to your location so you can check.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      Geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+
+          setLatitude(latitude);
+          setLongitude(longitude);
+        },
+        error => {
+          console.log(error.code, error.message);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 10000,
+          maximumAge: 10000,
+        },
+      );
+    } else {
+      Alert.alert('Permissions Denied');
+    }
+  };
+
+  useEffect(() => {
+    checkPermissions();
+  }, []);
+
   return (
     <View style={styles.MainContainer}>
       <MapView
@@ -21,8 +53,8 @@ const App = () => {
         zoomEnabled={true}
         zoomControlEnabled={true}
         initialRegion={{
-          latitude: 28.57966,
-          longitude: 77.32111,
+          latitude: latitude,
+          longitude: longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}>
